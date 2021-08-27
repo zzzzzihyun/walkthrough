3772b9c3455d14d5bd1888492fc6738fd13dd753
zzzzzihyun <zzzzzihyun@gmail.com>
2021-08-27 12:06:19 +0900

    commit

---
+++ Step21/webapp/component.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"./controller/HelloDialog"
+
+], function (UIComponent, JSONModel, HelloDialog) {
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata : {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest : "json"
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+			// set data model
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+		},
+
+
+		exit : function() {
+			this._helloDialog.destroy();
+			delete this._helloDialog;
+		},
+
+		openHelloDialog : function () {
+			this._helloDialog.open();
+		}
+	});
+});
